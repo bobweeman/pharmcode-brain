2cab4708ea7d1a0b86943b557bb83efd5b0d95d9
Bervelyn <bervelynboateng@gmail.com>
2018-04-13 12:11:54 +0000

    Create models

---
+++ app/Http/Controllers/DrugCategoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DrugCategory;
+use Illuminate\Http\Request;
+
+class DrugCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\DrugCategory  $drugCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DrugCategory $drugCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\DrugCategory  $drugCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DrugCategory $drugCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\DrugCategory  $drugCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DrugCategory $drugCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\DrugCategory  $drugCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DrugCategory $drugCategory)
+    {
+        //
+    }
+}
